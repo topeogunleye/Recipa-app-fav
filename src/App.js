7fe9d40f1e65867330bb4e25544281dd8840a697
Temitope Ogunleye <topeogunleye1@gmail.com>
2021-06-16 18:23:29 +0100

    Adds App js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import './App.css';
-import {Store} from ''
+import { Store } from './components/Store';
+import { StoreProvider } from './components/useFetchMealDbApi';
+import Home from './pages/Home';
+import { DarkModeProvider } from './contexts/DarkModeProvider';
+import AuthContextProvider from './contexts/AuthContext';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import MealInfo from './components/mealinfo/MealInfo';
+import Categories from './pages/Categories';
+import CategoryInfo from './pages/CategoryInfo';
+import RandomMeal from './pages/RandomMeal';
+import NewMealForm from './pages/newmealform/NewMealForm';
+import Loginsignup from './pages/loginsignup/Loginsignup';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <StoreProvider>
+        <DarkModeProvider>
+          <AuthContextProvider>
+            <div className="App">
+              <Home />
+            </div>
+          </AuthContextProvider>
+        </DarkModeProvider>
+      </StoreProvider>
+    </Router>
   );
 }
 
