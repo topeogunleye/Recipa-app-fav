0ed5ece3038803e08b52a3e6f1f4e44918b69e9e
Temitope Ogunleye <topeogunleye1@gmail.com>
2021-06-16 18:24:48 +0100

    Adds index js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { StoreProvider } from './components/Store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <StoreProvider>
       <App />
-    </StoreProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
